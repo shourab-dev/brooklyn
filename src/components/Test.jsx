c400edf88ed93a60c2606b27103eab431a2e54a4
shourab <shourab.webdev@gmail.com>
2025-03-12 15:11:02 +0600

    Version 4

---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <div>Hey I am test component</div>;
+};
+
+export default Test;
--- src/components/Version3.jsx
+++ src/components/Version3.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Version3 = () => {
   return (
     <div>
-        <h2>This is Version 3</h2>
+        <h2>This is Version 4</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita a earum fuga praesentium possimus nobis deleniti quos consequatur, explicabo corporis nisi facilis excepturi corrupti, sapiente illum alias aliquam? Eius, qui recusandae mollitia sequi perferendis explicabo? Est dolor eligendi repellendus aut, fuga nulla temporibus sed corporis, soluta exercitationem repudiandae, facilis tempore!</p>
     </div>
   )
 }
