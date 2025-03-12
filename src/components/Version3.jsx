94bd43c4a03f7553ce8014ede551faa2757e2da4
shourab <shourab.webdev@gmail.com>
2025-03-12 15:22:53 +0600

    link tag has been added

--- src/components/Version3.jsx
+++ src/components/Version3.jsx
@@ -5,6 +5,7 @@ const Version3 = () => {
     <div>
         <h2>This is Version 4</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita a earum fuga praesentium possimus nobis deleniti quos consequatur, explicabo corporis nisi facilis excepturi corrupti, sapiente illum alias aliquam? Eius, qui recusandae mollitia sequi perferendis explicabo? Est dolor eligendi repellendus aut, fuga nulla temporibus sed corporis, soluta exercitationem repudiandae, facilis tempore!</p>
+        <a href="#">Click me!</a>
     </div>
   )
 }
